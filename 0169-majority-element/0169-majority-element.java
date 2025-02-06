e1b6468a9234057589a48dd56d5c5cd873b13e30
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-02-06 14:17:42 -0500

    Time: 33 ms (7.18%), Space: 48.4 MB (99.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // boyer-moore voting algo
-        int candidate = nums[0];
-        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int num : nums) {
-            if (count == 0)
-                candidate = num;
-            count += (candidate == num) ? 1 : -1;
+            // if the int isn't a key already,
+            // make it a key associated with value 1
+            if (!map.containsKey(num))
+                map.put(num, 1);
+            // if the int is a key already
+            // increment value by 1
+            else
+                map.replace(num, map.get(num) + 1);
         }
-        
-        return candidate;
+        // now in our map we have an element with the highest occurrences
+        // find it, save it into int ans
+        int max = Collections.max(map.values());
+        // find the key that is associated with the value
+        final int[] returnVal = new int[1];
+        map.forEach( (k, v) -> {
+            System.out.println(k + " : " + v);
+            if (v == max)
+                returnVal[0] = k;
+        });
+        return returnVal[0];
     }
     
 }
