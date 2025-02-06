3a3e09ad0450daebbdde96e077278d46b39853df
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-02-06 14:19:25 -0500

    Time: 1 ms (51.41%), Space: 57.2 MB (84.73%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,23 +1,21 @@
 class Solution {
-        public static void rotate(int[] arr, int k) {
-        int count = 0;
-
-        for (int i = 0; count < arr.length; i++) {
-            int currentId = i;
-            int savedVal = arr[currentId];
-
-            do {
-                int nextId = loopIterator(currentId, k, arr.length);
-                int temp = arr[nextId];
-                arr[nextId] = savedVal;
-                savedVal = temp;
-                currentId = nextId;
-                count++;
-            } while (currentId != i);
-
+       public static void rotate(int[] arr, int k) {
+        k = k % arr.length;
+        if (k == 0 || arr.length <= 1) return;
+        // new array
+        int[] newArr = new int[arr.length];
+        int newArrIndex = 0;
+        // first array to be added is the subarray at arr.length-k to arr.length-1
+        for (int i = arr.length-k; i < arr.length; i++) {
+            newArr[newArrIndex++] = arr[i];
         }
-    }
-    public static int loopIterator(int currentId, int steps, int arrSize) {
-        return ((currentId + steps) % arrSize);
+        // second array to be added is the subarray at 0 to arr.length-k
+        for (int i = 0; i < arr.length-k; i++) {
+            newArr[newArrIndex++] = arr[i];
+        }
+
+        for (int i = 0; i < newArr.length; i++) {
+            arr[i] = newArr[i];
+        }
     }
 }
