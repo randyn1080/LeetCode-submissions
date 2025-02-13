abaa6123d4ada5a736197ece544813c26c2d2dd4
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-02-13 16:28:52 -0500

    Time: 1 ms (93.88%), Space: 45.2 MB (94.34%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,26 +3,23 @@ class Solution {
         // first get the sum of the entire array
         int totalSum = 0;
         for (int i : nums) totalSum += i;
-        // now we have the total sum, we need to work with the left and right sums for the problem
+        // we need to work with the left and right sums for the problem
         int rightSum = totalSum;
         int leftSum = 0;
         // now we loop through the array and 
         // if the left and right sum are equal, we return the id
-        // if we reach a spot where the leftsum is greater than the rightsum
-        // then we return -1 (false condition) since we cannot possibly have a 
-        // match in this scenario
         for (int i = 0; i < nums.length; i++) {
-            // set the pointer to 0 and in doing so
             // subtract el at one from the rightsum
-            // due to strictly clause
+            // due to strictly clause only hold what is right and left of pointer.
             rightSum -= nums[i];
             // now compare
             if (leftSum == rightSum) return i;
-            //if (leftSum > rightSum) return -1;
             // comparisons are done so now we need to add 
             // el at i to leftSum
             leftSum += nums[i];
         }
+        // if we make it out we didnt find a suitable pivot point
+        // return the false condition -1
         return -1;
     }
 }
