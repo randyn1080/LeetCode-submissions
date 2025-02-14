0255aa34d83d48c06e7c7c0754ae88f8ace2d993
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-02-14 16:19:18 -0500

    Time: 2 ms (88.09%), Space: 55.5 MB (62.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -8,22 +8,9 @@ class Solution {
             prefixArr[i+1] = currentPrefix;
             
         }
-        System.out.println("this is prefixArr");
-        for (int i : prefixArr) {
-            System.out.println(i);
-        }
-        // we now have [1, 1, 2, 6]
-        // we need     [24, 12, 8, 6]
-        // from calculating the suffix starting at the end of num
-        // is going to be 4, 12, 24
-        // so begin at index 1 from last (nums.length-2)
-        // and find the suffix product
-        // then make that position =
-        // suffix product * current position
         int suffixProduct = 1;
         int numsId = nums.length-1;
         for (int i = nums.length-2; i >= 0; i--) {
-            // calculate suffix
             suffixProduct *= nums[numsId--];
             prefixArr[i] *= suffixProduct;
         }
