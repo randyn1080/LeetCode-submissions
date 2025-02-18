f3a70c3907d92d514ada1aa52cf5c036354b2ce4
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-02-17 22:26:27 -0500

    Time: 3 ms (88.63%), Space: 46.4 MB (66.91%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,19 +1,20 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+        // sliding window
         int leftWall = 0;
         int rightWall = 0;
-        int sumOfWindow = 0;
+        int onesCount = 0;
         while (rightWall < nums.length) {
-            // first, increment the sum of the current window
-            sumOfWindow += nums[rightWall];
-            // then if the sum + k is >= our window length
-            if ( (sumOfWindow + k) >= (rightWall - leftWall + 1)) {
+            // first, add 1 if a 1 exists
+            onesCount += nums[rightWall];
+            // then if the onescount + k is >= our window length
+            if ( (onesCount + k) >= (rightWall - leftWall + 1)) {
                 // increase window length
                 rightWall++;
-            } else { // else the sumOfWindow+k is < windowLengght
+            } else { // else the sumOfWindow+k is < windowLenght
                 // move entire window right, recalculating sumOfWindow
                 // subtract the leftWall val from sumOfWindow
-                sumOfWindow -= nums[leftWall++];
+                onesCount -= nums[leftWall++];
                 rightWall++;
             }
         }
