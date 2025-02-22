f9b2e70039346633a0dc65cef5cda1d1dd2307ca
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-02-22 14:42:48 -0500

    Time: 0 ms (100%), Space: 42.6 MB (58.11%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+ /*
+
+[1, 2, 4]
+[1, 3, 4]
+
+iterate through the nodes until 
+
+
+
+[1, 1, 2, 3, 4, 4]
+
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+        ListNode newHead; 
+        ListNode list1Iter = list1;
+        ListNode list2Iter = list2;
+
+        if (list1Iter.val <= list2Iter.val) {
+            newHead = list1Iter;
+            list1Iter = list1Iter.next;
+        } else {
+            newHead = list2Iter;
+            list2Iter = list2Iter.next;
+        }
+        ListNode newHeadIter = newHead;
+        // our new head is determined and stored at newHead
+        // and the iterators are in place depending on which
+        // one is head (first loop is taken care of)
+
+        while (list1Iter != null && 
+               list2Iter != null) {
+            if (list1Iter.val <= list2Iter.val) {
+                newHeadIter.next = list1Iter;
+                newHeadIter = newHeadIter.next;
+                list1Iter = list1Iter.next;
+            } else {
+                newHeadIter.next = list2Iter;
+                newHeadIter = newHeadIter.next;
+                list2Iter = list2Iter.next;
+            }
+        }
+        if (list1Iter != null) {
+            while (list1Iter != null) {
+                newHeadIter.next = list1Iter;
+                newHeadIter = newHeadIter.next;
+                list1Iter = list1Iter.next;
+            }
+        } else if (list2Iter != null) {
+            while (list2Iter != null) {
+                newHeadIter.next = list2Iter;
+                newHeadIter = newHeadIter.next;
+                list2Iter = list2Iter.next;
+            }
+        }
+
+        return newHead;
+    }
+}
