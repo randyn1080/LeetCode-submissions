3bb86494b84631013a77645b6766da265e92e2a3
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-03-06 15:31:02 -0500

    Time: 4 ms (100%), Space: 43.5 MB (99.51%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean isPalindrome(int x) {
         // all negative numbers are false
-        String myInt = Integer.toString(x);
-        boolean isPal = true;
+        if (x < 0) return false;
+        // nums 0-9 are true
+        else if (x < 10) return true;
+        int reversedNum = 0;
+        int tempX = x;
 
-        int leftP = 0;
-        int rightP = myInt.length()-1;
-
-        while (leftP < rightP) {
-            if ( !(myInt.charAt(leftP) == myInt.charAt(rightP)) ){
-                return false;
-            }
-
-            leftP++;
-            rightP--;
+        while (tempX > 0) {
+            // get the first digit and put it at the left side of a new number
+            // so for 121, we would add 1, then turn 1 into 10, then add 2, then turn 12 into 120
+            // then add 1, then we would break because tempX will be 0 and we would have the reversed num
+            int remainder = tempX % 10;
+            reversedNum = (reversedNum*10) + remainder;
+            tempX = tempX / 10;
         }
 
-        return isPal;
+
+        return reversedNum == x;
     }
 }
