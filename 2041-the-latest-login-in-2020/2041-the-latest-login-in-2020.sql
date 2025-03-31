9952f08f19afa092193d89a24032c5ee5d48d36f
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-03-31 11:20:10 -0400

    Time: 933 ms (74.98%), Space: 0B (100%) - LeetHub

--- 2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT user_id, Max(time_stamp) AS last_stamp
 FROM logins
-WHERE time_stamp BETWEEN '2020-01-01 00:00:00' AND '2020-12-31 23:59:59'
+WHERE YEAR(time_stamp) = '2020'
 GROUP BY 1
