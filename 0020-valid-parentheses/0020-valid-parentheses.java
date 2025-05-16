c9dc542ca62c047c874c1fe38a3d2eadb5ee2045
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-05-16 13:42:46 -0400

    Time: 2 ms (97.26%), Space: 41.7 MB (65.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         Deque<Character> charStack = new ArrayDeque<>();
 
-        for (Character c : s.toCharArray()){
+        for (char c : s.toCharArray()){
             if (charStack.isEmpty()) {
                 // push current onto it
                 charStack.push(c);
@@ -10,13 +10,11 @@ class Solution {
                 // if it's not empty, but c is an open bracket, we can push
                 charStack.push(c);
             } else { // c is a closing bracket, the top must be it's complement.
-                char topOfStack = charStack.peek();
-                if (c == '}' && topOfStack == '{' ||
-                    c == ']' && topOfStack == '[' ||
-                    c == ')' && topOfStack == '(') {
-                    
-                    charStack.pop();
-                } else {
+                char topOfStack = charStack.pop();
+                if (c == '}' && topOfStack != '{' ||
+                    c == ']' && topOfStack != '[' ||
+                    c == ')' && topOfStack != '(') {
+                     
                     return false;
                 }
             }
