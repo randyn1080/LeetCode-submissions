31d6922263fe6b110db4ec2bc5dcc209ef9dae2c
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-05-16 13:28:58 -0400

    Time: 3 ms (41.5%), Space: 42.2 MB (8.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,39 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> parenthesesStack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            // check for ()[]{}
-            // push onto stack if ({[
-            if (s.charAt(i) == '(' ||
-                s.charAt(i) == '[' ||
-                s.charAt(i) == '{') {
-                
-                parenthesesStack.push(s.charAt(i));
-            }
-            else if (parenthesesStack.isEmpty() &&
-                    (s.charAt(i) == ')' ||
-                    s.charAt(i) == '}' ||
-                    s.charAt(i) == ']')) {
-                
-                return false;
-            }
-            else if (s.charAt(i) == ')' &&
-                    parenthesesStack.peek() == '(') {
-                parenthesesStack.pop();
-            }
-            else if (s.charAt(i) == '}' &&
-                    parenthesesStack.peek() == '{') {
-                parenthesesStack.pop();
-            }
-            else if (s.charAt(i) == ']' &&
-                    parenthesesStack.peek() == '[') {
-                parenthesesStack.pop();
-            } else {
-                parenthesesStack.push(s.charAt(i));
+        Deque<Character> charStack = new ArrayDeque<>();
+
+        for (Character c : s.toCharArray()){
+            if (charStack.isEmpty()) {
+                // push current onto it
+                charStack.push(c);
+            } else if (c == '{' || c == '[' || c == '('){
+                // if it's not empty, but c is an open bracket, we can push
+                charStack.push(c);
+            } else { // c is a closing bracket, the top must be it's complement.
+                char topOfStack = charStack.peek();
+                if (c == '}' && topOfStack == '{' ||
+                    c == ']' && topOfStack == '[' ||
+                    c == ')' && topOfStack == '(') {
+                    
+                    charStack.pop();
+                } else {
+                    return false;
+                }
             }
         }
 
-        return parenthesesStack.isEmpty();
-
+        return charStack.isEmpty();
     }
 }
