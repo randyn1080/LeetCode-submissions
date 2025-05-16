de9a68df3f912dc123041f350eaa51797aa57b6f
randyn1080 <59060386+randyn1080@users.noreply.github.com>
2025-05-16 15:24:13 -0400

    Time: 1 ms (63.98%), Space: 41.3 MB (87.59%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,34 +1,28 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb = new StringBuilder("");
-        // if the first string is the longest
-        // find the shortest first and base the rest off that
-        int minLength = Integer.MAX_VALUE;
-        for (int i = 0; i < strs.length; i++) {
-            minLength = Math.min(minLength, strs[i].length());
-        }
-        // search the arrays 
-        // look at the first letter of the first string
-        // save it as tempPrefix
-        // check every first letter of every string
-        // set sentinal value to false if we encounter a first letter that isnt
-        // the same as tempPrefix
-        // once we reach the last str, if sent val is true then add the temp prefix
-        // to the actual stringbuilder prefix. if it is false break the entire loop
-        // and return whatever is in sb.toString();
-        boolean sentVal = true;
-        for (int i = 0; i < minLength; i++) {
-            char tempPrefix = strs[0].charAt(i);
-            for (int j = 0; j < strs.length; j++) {
-                if (strs[j].charAt(i) != tempPrefix){
-                    sentVal = false;
-                    break;
+        // first we can sort the array which will give us an easier data set to work with
+        Arrays.sort(strs);
+        StringBuilder sb = new StringBuilder();
+        // now that strs array is sorted, the strings are organized lexigraphically 
+        // so we can logically compare each starting with the first to the last, 
+        // first will be the shortest string.
+
+        // compare the first letter.. if it's the same for each, add it to the result
+        // continue doing this for each position until we reach the end of the first word OR
+        // we run into a letter that isnt the same.
+        for (int i = 0; i < strs[0].length(); i++) {
+            // hold the current iteration of strs[0] letter in letter variable
+            char letter = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                // for each in strs, check that the letter matches
+                if (strs[j].charAt(i) != letter) {
+                    return sb.toString();
                 }
             }
-            if (sentVal) {
-                sb.append(strs[0].charAt(i));
-            } else break;
+            sb.append(letter);
         }
+
         return sb.toString();
     }
 }
